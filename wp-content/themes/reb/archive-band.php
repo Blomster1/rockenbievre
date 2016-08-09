a79d5a1eaa85b1776e60763a9d523118931408e8
root <ytabita@3c-e.com>
2016-08-09 17:21:40 +0200

    archive groupes

---
+++ wp-content/themes/reb/archive-band.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header();
+?>
+
+<section id="bands-gallery" class="container-fluid">
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6"></div>
+            </div>
+        </div>
+    </div>
+</section>
--- wp-content/themes/reb/header.php
+++ wp-content/themes/reb/header.php
@@ -38,46 +38,7 @@
             </ul>
         </nav>
 
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/LZuCfH0.jpg" alt="">
-                    <div class="carousel-caption">
-                        <div class="row">
-                            <div class="col-sm-12 text-center">
-                                <h1>Titre</h1>
-                                <p>description</p>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="" alt="">
-                    <div class="carousel-caption">
-                       balbalbla
-                    </div>
-                </div>
-                ...
-            </div>
-
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span class="" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span class="" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+        
 
 	</header><!-- .site-header -->
 	<div id="content" class="site-content">
